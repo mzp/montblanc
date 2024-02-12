b86ca8882a9578e8ccff44c17157020097d59dff
mzp <me@mzp.jp>
2024-02-11 21:25:29 -0800

    Initial Commit

---
+++ CaptureVision/CaptureVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureVisionApp.swift
+//  CaptureVision
+//
+//  Created by mzp on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CaptureVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
